bf0eb76b152b2cf265968afd6b97732540696ed4
MatweyL <matweyka2003@mail.ru>
2022-10-15 23:49:30 +0300

    found functionality for muscles entering

--- app/web/forms.py
+++ app/web/forms.py
@@ -1,10 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, FieldList, FormField, \
+    SelectField
 from wtforms.validators import DataRequired
+
+
+class MuscleForm(FlaskForm):
+    muscle_id = IntegerField('id')
+    name = SelectField(choices=["e", "2", "d"])
 
 
 class ExerciseForm(FlaskForm):
     exercise_id = IntegerField('id')
     name = StringField('Название', validators=[DataRequired()])
     description = StringField('Описание', validators=[DataRequired()])
+    muscles = FieldList(FormField(MuscleForm), min_entries=1, max_entries=2)
     submit = SubmitField('Сохранить')
